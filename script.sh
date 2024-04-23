a46f7075477afdf7ee65b80069ed9f0dd012358c
Chitresh Mishra <152098604+mishra221221@users.noreply.github.com>
2024-04-23 16:02:13 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 apt-get install python3-pip
 python3 manage.py makemigrations
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python3 manage.py migrate
+python3 manage.py runserver 0.0.0.0:8000
